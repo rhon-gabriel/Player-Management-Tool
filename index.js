eafbf7415b763d961ba656ebacead1160ab78173
rhon-gabriel <rhoneil.gabriel@appliedtechnology.se>
2021-02-07 11:34:49 +0100

    feat(player): add avatar on new created player

--- index.js
+++ index.js
@@ -21,17 +21,20 @@ app.listen(3001, () => console.log("app listening on port 3001."));
 
 /* The mock 'database' */
 let players = [
-  { id: "cjeodaus60000poul1g030oia", name: "Richard Garfield" },
-  { id: "cjeodaus60001poule3wjdz1p", name: "Gabe Newell" },
-  { id: "cjeodaus60000poul1g030oir", name: "Mario Sr" },
-  { id: "cjeodaus60001poule3wjdz1t", name: "Super Sonic" },
-  { id: "cjeodaus60001poule3wjdz1e", name: "Gabe Newell" },
-  { id: "cjeodaus60000poul1g030oig", name: "Mario Sr" },
+  { id: "cjeodaus60000poul1g030oia", name: "Ryu" },
+  { id: "cjeodaus60001poule3wjdz1p", name: "Chun Li" },
+  { id: "cjeodaus60000poul1g030oir", name: "Ken" },
+  { id: "cjeodaus60001poule3wjdz1t", name: "Vega" },
+  { id: "cjeodaus60001poule3wjdz1e", name: "Dee Jay" },
+  { id: "cjeodaus60000poul1g030oig", name: "Laura" },
+  { id: "cjeodaus60000poul1g030out", name: "Gammy" },
+  { id: "cjeodaus60000poul1g030opo", name: "Sakura" },
+  { id: "cjeodaus60000poul1g030oye", name: "Birdie" },
 ];
 
-players.map(el => {
-  el.avatar = generator.generateRandomAvatar()
-})
+players.map((el) => {
+  el.avatar = generator.generateRandomAvatar();
+});
 
 /* IMPLEMENTATION DETAILS */
 
@@ -57,7 +60,11 @@ function addPlayer(req, res) {
   if (!name) {
     return res.status(401).end();
   }
-  const newPlayer = { id: cuid(), name };
+  const newPlayer = {
+    id: cuid(),
+    name,
+    avatar: generator.generateRandomAvatar(),
+  };
   players = [...players, newPlayer];
   return res.status(201).json(newPlayer).end();
 }
